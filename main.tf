f226fd9605ad486beb130bf199efb851ac4f05e3
Anup Jacob <108464404+AnupJacob@users.noreply.github.com>
2024-04-29 21:02:14 +0100

    Adding instance to test and plan

--- main.tf
+++ main.tf
@@ -8,5 +8,17 @@ terraform {
 }
 
 provider "aws" {
-  region = var.aws-region
+  region = var.AWS-REGION
+
+#  access_key = var.AWS_ACCESS_KEY
+#  secret_key = var.AWS_SECRET_KEY
+}
+
+resource "aws_instance" "personal_test" {
+  ami           = "ami-0d421d84814b7d51c"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "test"
+  }
 }
