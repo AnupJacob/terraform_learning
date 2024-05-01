1725706f775ad971da63a893d21e4591f6519b5b
Anup Jacob <108464404+AnupJacob@users.noreply.github.com>
2024-05-01 17:24:15 +0100

    Replacing hard codes with variables

--- variables.tf
+++ variables.tf
@@ -1,4 +1,29 @@
-variable "aws-region" {
+variable "aws_region" {
   description = "Region for terraform configuration"
   default     = "eu-west-1"
+}
+
+variable "aws_availability_zone" {
+  description = "Availability zone for terraform configuration"
+  default     = "eu-west-1a"
+}
+
+variable "ec2_instance_type" {
+  description = "EC2 INSTANCE TYPE"
+  default     = "t2.micro"
+}
+
+variable "ec2_ami" {
+  description = "EC2 default AMI configuration"
+  default     = "ami-0d421d84814b7d51c"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR for VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  description = "CIDR for subnet"
+  default     = "10.0.1.0/24"
 }
