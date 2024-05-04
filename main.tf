5293c6d934c3a915cec018ff159ebdb062ae28c5
Anup Jacob <anupjacob@Anups-MacBook-Pro.local>
2024-05-04 10:12:42 +0100

    Updating terraform code learning

--- main.tf
+++ main.tf
@@ -9,8 +9,6 @@ terraform {
 
 provider "aws" {
   region     = var.aws_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
 }
 
 resource "aws_instance" "test_server_instance" {
@@ -53,5 +51,64 @@ resource "aws_subnet" "test_subnet2" {
 
   tags = {
     Name = "terraform test subnet 2"
+  }
+}
+
+resource "aws_route_table" "route_table_1" {
+  vpc_id = aws_vpc.test_vpc1.id
+
+  route = []
+
+  tags = {
+    Name = "example"
+  }
+}
+
+resource "aws_route_table" "route_table_2" {
+  vpc_id = aws_vpc.test_vpc2.id
+
+  route = []
+
+  tags = {
+    Name = "example"
+  }
+}
+
+resource "aws_route_table_association" "route_table_association_1" {
+  route_table_id = aws_route_table.route_table_1.id
+  gateway_id = aws_internet_gateway.internet_gateway_1.id
+}
+
+resource "aws_route_table_association" "route_table_association_2" {
+  route_table_id = aws_route_table.route_table_2.id
+  gateway_id = aws_internet_gateway.internet_gateway_2.id
+}
+
+resource "aws_internet_gateway" "internet_gateway_1" {
+  vpc_id = "${aws_vpc.test_vpc1.id}"
+  depends_on = [aws_vpc.test_vpc1]
+}
+
+resource "aws_internet_gateway" "internet_gateway_2" {
+  vpc_id = "${aws_vpc.test_vpc2.id}"
+  depends_on = [aws_vpc.test_vpc2]
+}
+
+resource "aws_budgets_budget" "budget-limit" {
+  name              = "budget-limit"
+  budget_type       = "COST"
+  limit_amount      = "1"
+  limit_unit        = "USD"
+  time_period_end   = "2050-06-15_00:00"
+  time_period_start = "2024-04-03_00:00"
+  time_unit         = "DAILY"
+
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 40
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "FORECASTED"
+    subscriber_email_addresses = ["anup.jacob@outlook.com"]
   }
 }
